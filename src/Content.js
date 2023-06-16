bf51f7a1db2a83486f83f4f1a1a71143f0db4573
Ahmed Shehata <47960845+ashehata8888@users.noreply.github.com>
2023-06-16 19:58:32 +0300

    some updates for build

--- src/Content.js
+++ src/Content.js
@@ -1,9 +1,9 @@
 // import images
 import Hero_person from "./assets/images/Hero/person.png";
 
-import figma from "./assets/images/Skills/figma.png";
-import sketch from "./assets/images/Skills/sketch.png";
-import ps from "./assets/images/Skills/ps.png";
+// import figma from "./assets/images/Skills/figma.png";
+// import sketch from "./assets/images/Skills/sketch.png";
+// import ps from "./assets/images/Skills/ps.png";
 import postgres from "./assets/images/Skills/postgres.png"
 import redux from "./assets/images/Skills/Redux.png"
 import ts from "./assets/images/Skills/ts.png"
@@ -33,26 +33,26 @@ import phpc from "./assets/images/certificates/photos/php.jpg"
 import next from "./assets/images/Skills/nextjs.png"
 import reactjs from "./assets/images/Skills/react.png";
 import nodejs from "./assets/images/Skills/node.png";
-import python from "./assets/images/Skills/python.png";
+// import python from "./assets/images/Skills/python.png";
 
 import services_logo1 from "./assets/images/Services/logo1.png";
 import services_logo2 from "./assets/images/Services/logo2.png";
-import services_logo3 from "./assets/images/Services/logo3.png";
+// import services_logo3 from "./assets/images/Services/logo3.png";
 
-import project1 from "./assets/images/projects/img1.png";
-import project2 from "./assets/images/projects/img2.png";
-import project3 from "./assets/images/projects/img3.png";
-import person_project from "./assets/images/projects/person.png";
+// import project1 from "./assets/images/projects/img1.png";
+// import project2 from "./assets/images/projects/img2.png";
+// import project3 from "./assets/images/projects/img3.png";
+// import person_project from "./assets/images/projects/person.png";
 
-import avatar1 from "./assets/images/Testimonials/avatar1.png";
-import avatar2 from "./assets/images/Testimonials/avatar2.png";
-import avatar3 from "./assets/images/Testimonials/avatar3.png";
-import avatar4 from "./assets/images/Testimonials/avatar4.png";
+// import avatar1 from "./assets/images/Testimonials/avatar1.png";
+// import avatar2 from "./assets/images/Testimonials/avatar2.png";
+// import avatar3 from "./assets/images/Testimonials/avatar3.png";
+// import avatar4 from "./assets/images/Testimonials/avatar4.png";
 
-import Hireme_person from "./assets/images/Hireme/person.png";
-import Hireme_person2 from "./assets/images/Hireme/person2.png";
+// import Hireme_person from "./assets/images/Hireme/person.png";
+// import Hireme_person2 from "./assets/images/Hireme/person2.png";
 
-import crm1 from "./assets/images/Portfolio/crm1.png"
+// import crm1 from "./assets/images/Portfolio/crm1.png"
 import crm2 from "./assets/images/Portfolio/crm2.png"
 import todo from "./assets/images/Portfolio/todo.png"
 import expense from "./assets/images/Portfolio/expense.png"
@@ -61,7 +61,7 @@ import rover from "./assets/images/Portfolio/rover.png"
 // import icons from react-icons
 import { GrMail } from "react-icons/gr";
 import { MdArrowForward, MdCall } from "react-icons/md";
-import { BsInstagram } from "react-icons/bs";
+// import { BsInstagram } from "react-icons/bs";
 import { FaLinkedin } from 'react-icons/fa';
 
 import { TbSmartHome } from "react-icons/tb";
@@ -298,63 +298,63 @@ export const content = {
       },
     ],
   },
-  Projects: {
-    title: "Projects",
-    subtitle: "MY CREATION",
-    image: person_project,
-    project_content: [
-      {
-        title: "Gym Website",
-        image: project1,
-      },
-      {
-        title: "Social Media web",
-        image: project2,
-      },
-      {
-        title: "Creative Website",
-        image: project3,
-      },
-    ],
-  },
-  Testimonials: {
-    title: "Testimonials",
-    subtitle: "MY CLIENT REVIEWS",
-    testimonials_content: [
-      {
-        review:
-          "“In publishing and graphic design, Lorem ipsum is a placeholder text commonly used to demonstra”",
-        img: avatar1,
-        name: "JOHN DOE",
-      },
-      {
-        review:
-          "“In publishing and graphic design, Lorem ipsum is a placeholder text commonly used to demonstra”",
-        img: avatar2,
-        name: "Tom Alex",
-      },
-      {
-        review:
-          "“In publishing and graphic design, Lorem ipsum is a placeholder text commonly used to demonstra”",
-        img: avatar3,
-        name: "Johnny",
-      },
-      {
-        review:
-          "“In publishing and graphic design, Lorem ipsum is a placeholder text commonly used to demonstra”",
-        img: avatar4,
-        name: "ROBBIN",
-      },
-    ],
-  },
-  Hireme: {
-    title: "Hire Me",
-    subtitle: "FOR YOUR PROJECTS",
-    image1: Hireme_person,
-    image2: Hireme_person2,
-    para: "In publishing and graphic design, Lorem ipsum is a placeholder text commonly used to demonstrate the visual form of a document elying on mean",
-    btnText: "Hire Me",
-  },
+  // Projects: {
+  //   title: "Projects",
+  //   subtitle: "MY CREATION",
+  //   image: person_project,
+  //   project_content: [
+  //     {
+  //       title: "Gym Website",
+  //       image: project1,
+  //     },
+  //     {
+  //       title: "Social Media web",
+  //       image: project2,
+  //     },
+  //     {
+  //       title: "Creative Website",
+  //       image: project3,
+  //     },
+  //   ],
+  // },
+  // Testimonials: {
+  //   title: "Testimonials",
+  //   subtitle: "MY CLIENT REVIEWS",
+  //   testimonials_content: [
+  //     {
+  //       review:
+  //         "“In publishing and graphic design, Lorem ipsum is a placeholder text commonly used to demonstra”",
+  //       img: avatar1,
+  //       name: "JOHN DOE",
+  //     },
+  //     {
+  //       review:
+  //         "“In publishing and graphic design, Lorem ipsum is a placeholder text commonly used to demonstra”",
+  //       img: avatar2,
+  //       name: "Tom Alex",
+  //     },
+  //     {
+  //       review:
+  //         "“In publishing and graphic design, Lorem ipsum is a placeholder text commonly used to demonstra”",
+  //       img: avatar3,
+  //       name: "Johnny",
+  //     },
+  //     {
+  //       review:
+  //         "“In publishing and graphic design, Lorem ipsum is a placeholder text commonly used to demonstra”",
+  //       img: avatar4,
+  //       name: "ROBBIN",
+  //     },
+  //   ],
+  // },
+  // Hireme: {
+  //   title: "Hire Me",
+  //   subtitle: "FOR YOUR PROJECTS",
+  //   image1: Hireme_person,
+  //   image2: Hireme_person2,
+  //   para: "In publishing and graphic design, Lorem ipsum is a placeholder text commonly used to demonstrate the visual form of a document elying on mean",
+  //   btnText: "Hire Me",
+  // },
   Contact: {
     title: "Contect Me",
     subtitle: "GET IN TOUCH",
